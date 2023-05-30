3a1b96e1b65a0bf9c08f5fc0229523b1aba5188c
jidoil <dotteda@gmail.com>
2023-05-30 17:20:52 +0900

    QMessageBox

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from PySide2.QtWidgets import QApplication
 import sys
-from main_widget import main_window
 
+from main_widget import main_window
+from theMessagBox import Widget
 
 app = QApplication(sys.argv)
 
 # window = RockWidget()
-window = main_window(app)
-
+# window = main_window(app)
+window = Widget()
 window.show()
 
 app.exec_()
